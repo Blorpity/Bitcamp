00ade4c3e8d28fd930bbcd9feabdff2e3f08fedd
Benji Leung-Arellano <tonatiuhula8@gmail.com>
2025-04-12 17:45:13 -0400

    UI :3

--- my-chrome-extension/src/App.tsx
+++ my-chrome-extension/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import sttLogo from './assets/SIgnToText.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
@@ -14,16 +14,14 @@ function App() {
       </div>
       <h1>SignToText</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button>
+          Activate/Deactivate Camera
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Click on the button to begin/end recording
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the button to begin recording
-      </p>
+
     </>
   )
 }
