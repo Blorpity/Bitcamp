da390bdeb18c8f5ecbb36e2e0232b402cf492edb
Benji Leung-Arellano <tonatiuhula8@gmail.com>
2025-04-12 16:09:56 -0400

    IT WORKSDKSHKC

--- my-chrome-extension/vite.config.ts
+++ my-chrome-extension/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
       input: {
         main: './index.html',
       },
-      external: ['/SignToText.png'],
+      external: ['/SignToText.svg'],
     },
   },
 });
